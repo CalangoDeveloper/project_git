d97b92809df40fc176624a89823e32c7a4286102
CalangoDeveloper <antonio.lustosa1@aluno.ce.gov.br>
2024-04-04 07:49:50 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
-void main(){}
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World!'),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
+          backgroundColor: const Color.fromARGB(255, 92, 92, 92),
+        ),
+      ),
+    );
+  }
+}
